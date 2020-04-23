2c39090273b3bee9be4b9842e514cafb7a919fb4
MohammedTarafdar <mtarafdar96@gmail.com>
2020-04-23 00:13:23 -0400

    First commit

---
+++ src/java/basic/DataTypes.java
@@ -0,0 +1,31 @@
+package java.basic;
+
+public class DataTypes {
+	
+		// Single line comments
+	
+		/*
+		* Multi-line comments
+		*/
+
+	public static void main(String[] args) {
+		
+		// Data types:
+		
+		//integer family:
+		//1. byte:
+			//size: 1 byte = 8 bits
+			//range: -128 to 127
+		
+		byte b =10;
+		byte b1 = -20;
+		byte b2 = 123;
+		
+		System.out.println(b);
+		//byte b2 = -123; Rule: Duplicate variable is not allowed in java.
+		b2 = -123;
+		
+
+	}
+
+}
