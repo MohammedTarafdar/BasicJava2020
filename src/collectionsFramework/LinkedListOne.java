f5d1f9db48fb68de1547b21530d6f216dd8ddb61
MohammedTarafdar <mtarafdar96@gmail.com>
2020-04-24 18:13:59 -0400

    LinkedListOne class updated

--- src/collectionsFramework/LinkedListOne.java
+++ src/collectionsFramework/LinkedListOne.java
@@ -1,5 +1,6 @@
 package collectionsFramework;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class LinkedListOne {
@@ -43,6 +44,60 @@ public class LinkedListOne {
 		
 		System.out.println("Content of LinkedList : "+ll);
 		System.out.println("");
+		
+		
+		// .removeFirst() and .removeLast()
+		
+		ll.removeFirst();
+		ll.removeLast();
+		
+		System.out.println("Content of LinkedList : "+ll);
+		System.out.println("");
+		
+		// .remove()
+		
+		ll.remove(2);
+		System.out.println("Content of LinkedList : "+ll);
+		System.out.println("");
+		
+		
+		/* How to print all values of LinkedList
+		 * 1. for loop
+		 * 2. advance for loop
+		 * 3. iterator
+		 * 4. while loop
+		 * 
+		 */
+		
+		
+		System.out.println("Using for loop :");
+		for(int n=0; n<ll.size(); n++) {
+			System.out.println(ll.get(n));
+		}
+		
+		System.out.println("");
+		System.out.println("Using Advanced for loop :");
+		for(String str :ll) {
+			System.out.println(str);
+		}
+		
+		System.out.println("");
+		System.out.println("Using iterator :");
+		
+		Iterator<String> it = ll.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+		
+		System.out.println("");
+		System.out.println("Using while loop :");
+		
+		int num = 0;
+		while(ll.size()>num) {
+			System.out.println(ll.get(num));
+			num++;
+		}
 		
 		
 		
