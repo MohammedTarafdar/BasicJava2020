bf10ba976ff969b98220a5ccaea2c881bca4f8a7
MohammedTarafdar <mtarafdar96@gmail.com>
2020-05-01 16:21:12 -0400

    ExceptionHandling class added

---
+++ src/javaWithTelusko/ExceptionHandling.java
@@ -0,0 +1,44 @@
+package javaWithTelusko;
+
+public class ExceptionHandling {
+	
+	/* Throwable
+	 * 		- Exception
+	 * 			- Checked like:
+	 * 				- IOException
+	 * 				- SQLException
+	 * 			
+	 * 			- Unchecked
+	 * 				- RuntimeException
+	 * 					- ArithmeticException
+	 * 					-
+	 * 					-
+	 * 
+	 * 		- Error
+	 */
+	
+	
+	public static void main(String[] args) {
+		
+		/* normal statement		like: i and j
+		 * critical statement	like: k
+		 */
+		
+		int i, j, k=0;
+		
+		i = 8;
+		j = 0;		// if i write 0 instead of 2, JVM will throw an ArithmeticException
+		
+		try {
+		k = i/j;
+		}
+		catch(ArithmeticException e){
+			System.out.println("Can not divided by Zero " + e);
+			
+		}
+		
+		System.out.println(k);
+		
+	}
+
+}
