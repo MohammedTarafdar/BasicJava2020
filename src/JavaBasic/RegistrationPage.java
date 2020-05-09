1a1ba3db556e6223629bf46fa10acc8ae177de61
MohammedTarafdar <mtarafdar96@gmail.com>
2020-05-09 14:01:52 -0400

    RegistrationPage and RegistrationTest classes added

---
+++ src/JavaBasic/RegistrationPage.java
@@ -0,0 +1,69 @@
+package JavaBasic;
+
+public class RegistrationPage {
+	
+	
+	String firstName;
+	String lastName;
+	long phoneNumber;
+	String emailID;
+	String password;
+	String Address;
+	String dob;
+	long dlNumber;
+	String gender;
+	boolean ageLimit;
+	int opt;
+	
+	// zero parameter constructor
+	
+	public RegistrationPage() {
+		System.out.println("Hi, this is zero parameter constructor");
+	}
+	
+	
+	// constructor with all filed
+	
+	public RegistrationPage(String firstName, String lastName, long phoneNumber, String emailID, String password,
+			String address, String dob, long dlNumber, String gender, boolean ageLimit, int opt) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.emailID = emailID;
+		this.password = password;
+		this.Address = address;
+		this.dob = dob;
+		this.dlNumber = dlNumber;
+		this.gender = gender;
+		this.ageLimit = ageLimit;
+		this.opt = opt;
+	}
+
+	
+	// constructor with mandatory fields
+
+	public RegistrationPage(String firstName, String lastName, long phoneNumber, String emailID, String password,
+			int opt) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.emailID = emailID;
+		this.password = password;
+		this.opt = opt;
+	}
+	
+	
+	public void doRegistration() {
+		System.out.println("processing registration with: "+firstName+" "+ lastName+ " "+password);
+	}
+
+	
+	public void getOTP() {
+		System.out.println(opt);
+	}
+	
+	
+
+}
---
+++ src/JavaBasic/RegistrationTest.java
@@ -0,0 +1,28 @@
+package JavaBasic;
+
+public class RegistrationTest {
+	
+	
+
+	public static void main(String[] args) {
+
+		
+		
+		RegistrationPage reg = new RegistrationPage();
+		
+		
+		RegistrationPage regAll = new RegistrationPage("Md", "Rahman", 347337992, "rahman@gmail.com", "test1234", "22 metropolitan Ave NY", 
+				"03-25-1985", 234567291, "Male", true, 9876);
+		
+		System.out.println(regAll.firstName+" "+regAll.lastName);
+		
+		
+		regAll.doRegistration();
+		
+		regAll.getOTP();
+
+	}
+	
+	
+
+}
