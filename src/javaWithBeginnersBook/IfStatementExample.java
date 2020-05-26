723ccf3043db03324b3ecbfc2a4095220f4746e5
MohammedTarafdar <mtarafdar96@gmail.com>
2020-05-26 15:53:16 -0400

    Java with Beginners Book package created

---
+++ src/javaWithBeginnersBook/IfStatementExample.java
@@ -0,0 +1,49 @@
+package javaWithBeginnersBook;
+
+public class IfStatementExample {
+
+	public static void main(String[] args) {
+
+		int num = 70;
+		if(num< 100) {
+			System.out.println("Our input "+num+ " is less then 100");
+		}
+		
+		
+		
+		int num1 = 75;
+		
+		//num1 =65;
+		
+		if(num1 >= 70 && num1 <= 100) {
+			System.out.println("Congratulation you got pass mark");
+		}
+		
+		
+
+	}
+	
+	
+	
+	
+
+}
+
+
+/*class FirstExample{
+	
+	int num1 = 65;
+	
+	
+	
+	if(num1 >= 70 && num1 <= 100) {
+		System.out.println();
+	}
+}
+*/
+
+
+
+
+
+
---
+++ src/javaWithBeginnersBook/NestedIfExample.java
@@ -0,0 +1,51 @@
+package javaWithBeginnersBook;
+
+public class NestedIfExample {
+
+	public static void main(String[] args) {
+
+		
+		// First example
+		int num = 75;
+		
+		if (num <100 ) {
+			
+			System.out.println("User input less then 100");
+			
+			if(num > 50) {
+				System.out.println("But it's more then 50 ");
+			}
+		}
+		
+	
+		
+		// Second example 
+		
+		int marks = 65;
+		marks =85;
+		marks = 25;
+		
+		if(marks >= 33) {
+			if(marks <45) {
+				System.out.println("you got third class marks");
+			}
+			
+			if(marks <60) {
+				System.out.println("you got second class marks");
+			}
+			
+			if(marks <80) {
+				System.out.println("you got first class marks");
+			}
+			
+			
+			if(marks >= 80 && marks <=100) {
+				System.out.println("you got star marks");
+			}
+		}
+		
+		System.out.println("Sorry you need to try again");
+
+	}
+
+}
