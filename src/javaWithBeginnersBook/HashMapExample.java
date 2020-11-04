63c2169bb033d4fe9cee0c67b96dd887bcc86c9d
MohammedTarafdar <mtarafdar96@gmail.com>
2020-11-03 23:34:44 -0500

    Hash map practice

---
+++ src/javaWithBeginnersBook/HashMapExample.java
@@ -0,0 +1,34 @@
+package javaWithBeginnersBook;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		Map<Integer, String> hMap= new HashMap<Integer, String>();
+		
+		hMap.put(1, "Apple");
+		hMap.put(2, "Banana");
+		hMap.put(3, "Orange");
+		hMap.put(4, "Nut");
+		
+		System.out.println("Value of Hash Map = "+hMap);
+		System.out.println(" ");
+		
+		// To get value based on key
+		String secondValueString =hMap.get(2);
+		System.out.println(secondValueString);
+		
+		System.out.println("Iterate through for loop =====>");
+		
+		for(Map.Entry<Integer, String> me: hMap.entrySet()) {
+			System.out.println("Key :"+me.getKey()+" Value :"+me.getValue());
+		}
+			
+		
+		
+
+	}
+
+}
